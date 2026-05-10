6973886ec5a017d4a35513f39bfb5a4895de34c0
Bishwajit Paul <paul.bishwajit09@gmail.com>
2026-05-10 10:04:09 +0600

    Committed after UI update

--- server/utils/database.js
+++ server/utils/database.js
@@ -2,9 +2,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 let db;
 
 async function connectDB() {
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l3p6wcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-  const client = new MongoClient(uri, {
+  const client = new MongoClient(process.env.MONGODB_URI, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
@@ -14,6 +12,7 @@ async function connectDB() {
 
   await client.connect();
   db = client.db("portfolio-crud");
+
   console.log("Connected to MongoDB successfully!");
 }
 
@@ -25,3 +24,31 @@ function getDB() {
 }
 
 module.exports = { connectDB, getDB };
+
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// let db;
+
+// async function connectDB() {
+//   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l3p6wcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+//   const client = new MongoClient(uri, {
+//     serverApi: {
+//       version: ServerApiVersion.v1,
+//       strict: true,
+//       deprecationErrors: true,
+//     },
+//   });
+
+//   await client.connect();
+//   db = client.db("portfolio-crud");
+//   console.log("Connected to MongoDB successfully!");
+// }
+
+// function getDB() {
+//   if (!db) {
+//     throw new Error("Database not connected. Please call connectDB first.");
+//   }
+//   return db;
+// }
+
+// module.exports = { connectDB, getDB };
