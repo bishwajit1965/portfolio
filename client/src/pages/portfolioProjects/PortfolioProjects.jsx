45e9a527cb20d8086a825ed99853c29dffa6f72f
Bishwajit Paul <paul.bishwajit09@gmail.com>
2026-05-12 00:21:59 +0600

    Committed after localhost fix

--- client/src/pages/portfolioProjects/PortfolioProjects.jsx
+++ client/src/pages/portfolioProjects/PortfolioProjects.jsx
@@ -312,7 +312,7 @@ const PortfolioProjects = () => {
                     </div>
 
                     {/* Links */}
-                    <div className="flex lg:gap-4 gap-2 m7-4">
+                    <div className="flex lg:gap-4 gap-2 my-4">
                       {project.githubLink && (
                         <a
                           href={project.githubLink}
