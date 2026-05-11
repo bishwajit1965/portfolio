09cc7ca8b9ab766b91fdd200f5882c8f5700e1ef
Bishwajit Paul <paul.bishwajit09@gmail.com>
2026-05-11 14:29:25 +0600

    Committed after localhost fix

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,13 @@
-const DEV_URL = "http://localhost:5000/api";
-const PROD_URL = "https://portfolio-wcsw.onrender.com/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const API_BASE_URL =
-  import.meta.env.MODE === "development" ? DEV_URL : PROD_URL;
+if (!API_BASE_URL) {
+  throw new Error("API_BASE_URL missing in env");
+}
+
+export { API_BASE_URL };
+
+// const DEV_URL = "http://localhost:5000/api";
+// const PROD_URL = "https://portfolio-wcsw.onrender.com/api";
+
+// export const API_BASE_URL =
+//   import.meta.env.MODE === "development" ? DEV_URL : PROD_URL;
